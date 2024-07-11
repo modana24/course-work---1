bf0ff0944182aa3624b72fd9d077b88ce98b5318
hime <dasha.motry2002@mail.ru>
2024-07-11 20:48:02 +0300

    готово

--- src/Employee.java
+++ src/Employee.java
@@ -1,5 +1,64 @@
 import java.util.Objects;
 
 public class Employee {
+        private String fullName;
+        private int salary;
+        private int departmentNumber;
 
-}
+        private int id;
+        private static int counter = 0;
+
+        public Employee(String fullName, int salary, int departmentNumber) {
+            this.fullName = fullName;
+            this.salary = salary;
+            this.departmentNumber = departmentNumber;
+
+            ++counter;
+            id = counter;
+        }
+
+        public String getFullName() {
+            return fullName;
+        }
+
+        public void setFullName(String fullName) {
+            this.fullName = fullName;
+        }
+
+        public int getSalary() {
+            return salary;
+        }
+
+        public void setSalary(int salary) {
+            this.salary = salary;
+        }
+
+        public int getDepartmentNumber() {
+            return departmentNumber;
+        }
+
+        public void setDepartmentNumber(int departmentNumber) {
+            this.departmentNumber = departmentNumber;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Employee employee = (Employee) o;
+            return Double.compare(salary, employee.salary) == 0 && departmentNumber == employee.departmentNumber && Objects.equals(fullName, employee.fullName);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(fullName, salary, departmentNumber);
+        }
+
+        @Override
+        public String toString() {
+            return "ФИ: " + fullName + ". "
+                    + "Зарплата: " + salary + ". "
+                    + "Отдел: " + departmentNumber + ". "
+                    + "Id: " + id;
+        }
+    }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,67 @@
 public class Main {
     public static void main(String[] args) {
 
-    }
-}
+                Employee [] employeesInfo = {
+                        new Employee("Иванов Иван",10_000,1),
+                        new Employee("Петров Петр", 11_000, 1),
+                        new Employee("Денисов Денис",12_000,3),
+                        new Employee("Артемов Артем", 13_000, 4),
+                        new Employee("Павлов Павел", 14_000, 5),
+                        new Employee("Вадимов Вадим", 15_000, 1),
+                        new Employee("Максимов Максим", 16_000,2),
+                        new Employee("Дмитров Дмитрий", 17_000,3),
+                        new Employee("Ильин Илья", 18_000,4),
+                        new Employee("Артуров Артур",19_000,5),
+                };
+                System.out.println("Общая сумма выплат сотрудникам составляет: " + salarySum(employeesInfo));
+                System.out.println("Минимальная зарплата в компании составляет: " + findMinSalary(employeesInfo));
+                System.out.println("Максимальная зарплата в компании составляет: " + findMaxSalary(employeesInfo));
+                System.out.println("Средняя зарплата в компании составляет: " + averageSalary(employeesInfo));
+
+                System.out.println();
+                System.out.println("Сотрудники компании:");
+                for (int i = 0; i < employeesInfo.length; i++) {
+                    System.out.println(employeesInfo[i]);
+                }
+
+            }
+
+            private static int salarySum(Employee [] employeesInfo) {
+                int sum = 0;
+                for (int i = 0; i < employeesInfo.length; i++) {
+                    sum += employeesInfo[i].getSalary();
+                }
+                return sum;
+            }
+
+            private static int findMinSalary(Employee [] employeesInfo) {
+                int minSalary = employeesInfo[0].getSalary();
+                for (int i = 0; i < employeesInfo.length; i++) {
+                    if (employeesInfo[i].getSalary() < minSalary) {
+                        minSalary = employeesInfo[i].getSalary();
+                    }
+                }
+                return minSalary;
+            }
+
+            private static int findMaxSalary(Employee [] employeesInfo) {
+                int maxSalary = employeesInfo[0].getSalary();
+                for (int i = 0; i < employeesInfo.length; i++) {
+                    if (employeesInfo[i].getSalary() > maxSalary) {
+                        maxSalary = employeesInfo[i].getSalary();
+                    }
+                }
+                return maxSalary;
+            }
+
+            private static int averageSalary(Employee [] employeesInfo) {
+                int difference = 0;
+                int sum = 0;
+                for (int i = 0; i < employeesInfo.length; i++) {
+                    sum += employeesInfo[i].getSalary();
+                }
+                difference = sum / employeesInfo.length;
+                return difference;
+            }
+        }
+
